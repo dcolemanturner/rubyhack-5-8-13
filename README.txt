8d15b3acded39ec3173d34fd53571d2383d8b80a
dcolemanturner <dcolemanturner@hotmail.com>
2013-05-08 20:20:39 -0500

    First commit of Ruby Hack project

---
+++ README.txt
@@ -0,0 +1,5 @@
+Solution for the DEAth of IFs.
+
+Developed By: 
+	Derrick Coleman-Turner
+	Will Smith
---
+++ if.rb
@@ -0,0 +1,47 @@
+#NO IF NO CASE
+#Create Hash with name / value pair - DONE
+#Iterate with do loop
+# puts valud
+
+def process(input)
+	if input == 'q'
+		puts 'Goodbye'
+	elsif input == 'tweet'
+		puts 'tweeting'
+	elsif input == 'dm'
+		puts 'direct messaging'
+	elsif input =='help'
+		puts 'helping'
+	end
+end
+
+def new_process(input)
+	noifs = { 
+			"q" => "Goodbye",
+            "tweet" => "tweeting",
+            "dm" => "direc messaging",
+            "help" => "helping"                      
+          }
+	noifs.default = ("I aint got nuttin for ya brah!!")
+
+	puts noifs[input]
+
+	#puts noifs.select {|noif| noif == input }
+
+=begin
+	for noifs.each do |noif|
+	   puts "Value of local variable is #{noif}"
+	end
+    #puts "boo boo" + noifs["q"]
+=end
+end
+
+
+process 'q'
+new_process 'q'
+
+
+
+
+
+
